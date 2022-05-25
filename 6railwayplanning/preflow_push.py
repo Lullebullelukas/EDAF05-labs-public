7ba9e4482884e5029ff3500cfe95e82fd5bae343
jakobohenning <jakob.henningsson@gmail.com>
2022-05-25 15:44:35 +0200

    leeesssgooo

--- 6railwayplanning/preflow_push.py
+++ 6railwayplanning/preflow_push.py
@@ -22,26 +22,45 @@ class Graph:
         self.drain = drain
         self.excesses = []
 
+def print_graph(graph):
+    print("-------Nodes-------")
+    for node in graph.nodes:
+        print("Node ", node.idx, "with e = ", node.excess)
+    print("-------Edges-------")
+    for key in graph.edges:
+        edge = graph.edges[key]
+        print(edge.node_u.idx, "->", edge.node_v.idx, "with c = ", edge.capacity, "and f = ", edge.flow)
+
 def can_push(node, graph):
     for neighbour in node.neighbours:
-        if node.height > neighbour.height:
+        if node.idx < neighbour.idx:
+            forward = 1
+            edge = graph.edges[node.idx, neighbour.idx]
+        else:
+            forward = -1
+            edge = graph.edges[neighbour.idx, node.idx]
+        if node.height > neighbour.height and forward * edge.flow < edge.capacity:
             return neighbour
     return None
 
 def push(graph, node_u, node_v):
+    # print("Pushing from ", node_u.idx, "to ", node_v.idx)
     if node_u.idx < node_v.idx:
         edge = graph.edges[node_u.idx, node_v.idx]
         delta = min(node_u.excess, edge.capacity-edge.flow)
+        # print("With delta = ", delta)
         edge.flow += delta
     else:
         edge = graph.edges[node_v.idx, node_u.idx]
-        delta = min(node_u.excess, edge.flow)
+        delta = min(node_u.excess, edge.flow + edge.capacity)
+        # print("With delta = ", delta)
         edge.flow -= delta
     node_u.excess -= delta
     node_v.excess += delta
 
 def relabel(node_u):
-    node_u.h += 1
+    # print("Relabeling ", node_u.idx)
+    node_u.height += 1
 
 def node_with_excess(graph):
     for i in range(1, graph.N-1):
@@ -54,13 +73,13 @@ def preflow_push(graph):
     source = graph.nodes[graph.source]
     source.height = graph.N
     for node in source.neighbours:
-        print("enters shit")
         edge = graph.edges[(source.idx, node.idx)]
         edge.flow = edge.capacity 
         edge.node_v.excess = edge.capacity 
         # graph.excesses.append(edge.node_v)
         source.excess -= edge.capacity
     while node_with_excess(graph) != None:
+        # print_graph(graph)
         node = node_with_excess(graph)
         neighbour = can_push(node, graph)
         if neighbour != None:
@@ -79,17 +98,16 @@ def main():
 
     for i in range(M):
         u, v, c = map(int, input().split(" "))
-        node_u = Node(u)
-        node_v = Node(v)
+        node_u = nodes[u]
+        node_v = nodes[v]
         edge = Edge(node_u, node_v, c)
         edges[(u,v)] = edge
         nodes[u].neighbours.append(node_v)
         nodes[v].neighbours.append(node_u)
 
     graph = Graph(N, M, nodes, edges, 0, N-1)
-    graph2 = preflow_push(graph)
-    for key in graph2.edges:  
-        print(graph2.edges[key].node_u.idx, graph2.edges[key].node_v.idx)
-        print(graph2.edges[key].flow)        
+    preflow_push(graph)
+    # print_graph(graph)
+    print("Excess i drain =", graph.nodes[graph.drain].excess)   
 
 main()
